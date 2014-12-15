2d81ce1f214a73d798f00c8b5dcb5e8f14699d0a
Krish Rajagopalan <krish.andres@gmail.com>
2014-12-15 09:38:52 -0800

    Create basic view for individual schedule

--- server.rb
+++ server.rb
@@ -17,3 +17,19 @@ get '/' do
   @schedules = XTask::ScheduleRepo.new.find_all
   erb :index
 end
+
+post '/schedules' do
+  puts params
+  @name = params[:scheduleName]
+  @username = params[:userName]
+  binding.pry
+  @schedule = XTask::ScheduleRepo.new.create({name: @name, username: @username})
+  redirect to("/schedules/#{@schedule.id}")
+end
+
+get '/schedules/:id' do
+  @schedule = XTask::ScheduleRepo.new.find(params[:id])
+  @tasks = XTask::TaskRepo.new.find_all(schedule: @schedule)
+
+  erb :schedule
+end
