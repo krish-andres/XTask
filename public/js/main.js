2b0781d8b25806220cabb47fc5cc6a8b4ef6c960
Connor Kojimoto <cokojimoto@gmail.com>
2014-12-16 14:25:50 -0800

    updated schedule page

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,24 @@
-var $div = $('<div class="alert-success j-form" role="alert"><strong>Well done!</strong> You successfully added a Form!</div>');
-var overlay = $($div).css({
-    position: "absolute",
-    width: "100%",
-    height: "80%",
-    left: 0,
-    bottom: 0,
-    zIndex: 500,  // to be on the safe side
-    "background-color": '#444'
-}).appendTo($(".container-form").css("position", "relative"));
-overlay.hide();
+// var $success-div = $('<div class="alert-success j-form" role="alert"><strong>Well done!</strong> You successfully added a Form!</div>');
+// var $fail-div = $'<div class="alert-danger j-form" role="alert"><strong>Im</strong> You successfully added a Form!</div>');
+// var overlay = $($div).css({
+//     position: "absolute",
+//     width: "100%",
+//     height: "80%",
+//     left: 0,
+//     bottom: 0,
+//     zIndex: 500,  // to be on the safe side
+//     "background-color": '#444'
+// }).appendTo($(".container-form").css("position", "relative"));
+// overlay.hide();
 
-$('.container-form').on('click', function() {
-  overlay.show().delay(1000).fadeOut('slow');
-});
+// $('.container-form').on('click', function() {
+//   overlay.show().delay(1000).fadeOut('slow');
+// });
   
+// $('.container-content').hide();
+// $('.container-form').on('click','.btn-primary',function() {
+//   if (!$('.container-content').is(":visible")) {
+//     $('.container-content').show();
+//   }
+  
+// });
